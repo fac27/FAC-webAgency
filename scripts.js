b44068d1859b878c494fd6be534fa97e3030d47e
FomasTreeman <tom@team-freeman.com>
2023-01-26 15:59:26 +0000

    fixed radio buttons'

--- scripts.js
+++ scripts.js
@@ -24,14 +24,25 @@ function openPest(evt, pestName) {
 function givePrice(event) {
     event.preventDefault();
     var elements = event.target.elements;
-    let problemPests = ['cockroaches', 'rats', 'ooze'].filter((pest) => elements[pest].checked == true)
-    console.log(problemPests);
+    console.log(elements.rat.value);
+    var ratPrice, cockPrice;
+    elements.rat.value == 'ratR' ? ratPrice = elements.quantity1.value * 200 : ratPrice = elements.quantity1.value * 500
+    elements.cock.value == 'cockR' ? cockPrice = elements.quantity2.value * 200 : cockPrice = elements.quantity2.value * 500
+    let oozePrice = elements.quantity3.value * 3000; 
+    var quantity = ratPrice + cockPrice + oozePrice;
+    console.log(quantity);
+    document.getElementsByTagName('output')[0].value = '= £' + quantity;
 };
 
-function askForQuantity(pest) {
-    document.getElementById('quantityField').style.display = 'flex';
-    // check if quantity field isnt showing 
-    document.getElementById('labelQuantity1').innerHTML = pest;
+// function askForQuantity(pest) {
+//     document.getElementById('quantityField').style.display = 'flex';
+//     // check if quantity field isnt showing 
+//     document.getElementById('labelQuantity1').innerHTML = pest;
 
-    document.getElementById("rats").style.display = "block";
+//     document.getElementById("rats").style.display = "block";
+// }
+
+function toggleForms(show, hide) {
+    document.getElementById(show).style.display = 'block';
+    document.getElementById(hide).style.display = 'none';
 }
